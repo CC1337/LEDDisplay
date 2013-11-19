4b7d6f4106b84fcc278779ec7390f73b922b5807
CC <t@schuett.cc>
2013-11-19 22:26:16 +0100

    added slide effect for temperatures

--- src/effects/text/InfoTextEffect.java
+++ src/effects/text/InfoTextEffect.java
@@ -20,6 +20,10 @@ public class InfoTextEffect implements IColorableEffect {
 	private int _posY;
 	private State _state = State.Temperatures;
 	private int _stateProgress = 0;
+	private boolean _switchToNextInfo = false;
+	private int _animationState = 0;
+	private byte[][] _prevStateData;
+	private byte[][] _nextStateData;
 	private NetIO1OG _netIO1OGData = NetIO1OG.getInstance();
 	
 	public InfoTextEffect(IPixelatedFont font, IColor color, int posX, int posY) {
@@ -27,7 +31,7 @@ public class InfoTextEffect implements IColorableEffect {
 		setPosX(posX);
 		setPosY(posY);
 		_color = color;
-		updateData();
+		processState();
 		nextInfo();
 	}
 	
@@ -35,30 +39,75 @@ public class InfoTextEffect implements IColorableEffect {
 		this(font, color, 0, 0);
 	}
 	
-	
-	public void updateData() {
-		
+	public void nextInfo() {
+		_switchToNextInfo = true;
 	}
 	
-	public void nextInfo() {
+	private void nextFrame() {
+		if (_animationState > 0)
+			changeAnimation();
+		
+		if (!_switchToNextInfo)
+			return;
+		_switchToNextInfo = false;
+		processState();	
+	}
+	
+	private void processState() {
 		if (_state == State.Temperatures) {
-			if (_stateProgress == 0)
-				_textArray = _font.toPixels("Au: " + _netIO1OGData.getTempAussenKueche() + "\u00BA");
-			if (_stateProgress == 1)
-				_textArray = _font.toPixels("SZ: " + _netIO1OGData.getTempSchlafzimmer() + "\u00BA");
-			if (_stateProgress == 2)
-				_textArray = _font.toPixels("WZ: " + _netIO1OGData.getTempWohnzimmer() + "\u00BA");
-			if (_stateProgress == 3)
-				_textArray = _font.toPixels("AQ: " + _netIO1OGData.getTempAquarium() + "\u00BA");
-			if (_stateProgress == 4)
-				_textArray = _font.toPixels("Ba: " + _netIO1OGData.getTempBalkon() + "\u00BA");
-			
-			_stateProgress++;
-			if (_stateProgress >= 5) 
-				_stateProgress = 0;
+			_prevStateData = _nextStateData;
+			if (_stateProgress == 0) {
+				_nextStateData = _font.toPixels("Au: " + _netIO1OGData.getTempAussenKueche() + "\u00BA");
+				_animationState = 1;
+			}
+			if (_stateProgress == 1) {
+				_nextStateData = _font.toPixels("SZ: " + _netIO1OGData.getTempSchlafzimmer() + "\u00BA");
+				_animationState = 1;
+			}
+			if (_stateProgress == 2) {
+				_nextStateData = _font.toPixels("WZ: " + _netIO1OGData.getTempWohnzimmer() + "\u00BA");
+				_animationState = 1;
+			}
+			if (_stateProgress == 3) {
+				_nextStateData = _font.toPixels("AQ: " + _netIO1OGData.getTempAquarium() + "\u00BA");
+				_animationState = 1;
+			}
+			if (_stateProgress == 4) {
+				_nextStateData = _font.toPixels("Ba: " + _netIO1OGData.getTempBalkon() + "\u00BA");
+				_animationState = 1;
+			}
+
+			_stateProgress = ++_stateProgress % 5;
 		}
 	}
 	
+	private void changeAnimation() {
+		if (_textArray == null || _prevStateData == null)
+			return;
+		
+		if (_state == State.Temperatures) {
+			for(int x=0; x<_textArray.length; x++) {
+				for(int y=0; y<_textArray[0].length; y++) {
+					if (x >= _nextStateData.length || x >= _prevStateData.length) {
+						_textArray[x][y] = 0;
+					} else {
+						//System.out.print("x: "+x + "  y: " + y + "  as: " + _animationState + "  ta0len: " + _textArray[0].length );
+						
+						if (y + _animationState >= _textArray[0].length) { 
+							//System.out.println(" c1 " + (-_textArray[0].length + y + _animationState));
+							_textArray[x][y] = _nextStateData[x][-_textArray[0].length + y + _animationState];
+						} else {
+							//System.out.println(" c2 " + (y + _animationState));
+							_textArray[x][y] = _prevStateData[x][y + _animationState];
+						}
+							
+					}
+				}
+			}
+			_animationState = ++_animationState % 8;
+		}
+		
+	}
 	
 	public int getPosX() {
 		return _posX;
@@ -78,6 +127,11 @@ public class InfoTextEffect implements IColorableEffect {
 
 	@Override
 	public void apply(ILEDArray leds) {
+		if (_textArray == null) {
+			_textArray = new byte[leds.sizeX()][_font.toPixels(" ")[0].length];
+			_prevStateData = new byte[leds.sizeX()][_font.toPixels(" ")[0].length];
+		}
+		nextFrame();
 		_color.apply(leds, this);
 	}
 
