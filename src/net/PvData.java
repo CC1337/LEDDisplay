2eeff4694a6362740b83403707cc85b54aade9d4
CC <t@schuett.cc>
2016-04-03 23:04:28 +0200

    remove unused imports

--- src/net/PvData.java
+++ src/net/PvData.java
@@ -1,14 +1,10 @@
 package net;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -31,7 +27,7 @@ public class PvData {
         return _instance;
     }
     
-    
+
     public int getPac() {
     	updateDayData();
     	
