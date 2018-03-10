d3b52d977aa53358034923d4523d5d0b282aeaa9
CC <t@schuett.cc>
2018-03-10 23:03:54 +0100

    fix rebootd restart command to work also if no display software running

--- rebootd/rebootd_config.php
+++ rebootd/rebootd_config.php
@@ -5,7 +5,7 @@ abstract class Config {
     const PinRebootButton = 38; // Physical pin number https://www.elektronik-kompendium.de/sites/raspberry-pi/1907101.htm
     const ShortPressMs = 70;
     const LongPressMs = 2000;
-    const RestartDisplayCommand = "/home/display/LEDDisplay/stop_display && /home/display/LEDDisplay/run_silent";
+    const RestartDisplayCommand = "/home/display/LEDDisplay/run_silent";
 
 }
 
